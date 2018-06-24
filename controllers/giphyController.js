1db29deca04239a37bb1281803d0540fc9d378ce
Marujinho <aka.contato@gmail.com>
2018-06-23 21:02:04 -0300

    login

--- controllers/giphyController.js
+++ controllers/giphyController.js
@@ -1,5 +1,9 @@
 geniusApp.controller('giphyController', function($scope, $state, $compile) {
 
+    if(localStorage.getItem('patronId') == "" || localStorage.getItem('patronId') == null){
+        $state.go('register');
+    }
+
     $scope.name = localStorage.getItem('patronName');
 
 });
