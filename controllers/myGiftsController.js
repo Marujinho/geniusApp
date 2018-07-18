5739b3122c4710941a342b856a1d5b8432bab840
Marujinho <aka.contato@gmail.com>
2018-07-17 21:53:14 -0300

    animacao dos presentes

--- controllers/myGiftsController.js
+++ controllers/myGiftsController.js
@@ -7,7 +7,29 @@ geniusApp.controller('myGiftsController', function($scope, $state, $compile, reg
     $scope.name = localStorage.getItem('patronName');
     $scope.id = localStorage.getItem('patronId');
     
-    
+    $scope.refuseGift = function(){
+
+        swal({
+            title: 'Certeza?',
+            text: "Ao jogar esse convite fora você nao poderá mais recuperá-lo depois",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Não quero mais, obrigado!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                swal({
+                    title: 'Ok, Voce descartou o presente',
+                }).then(function(){
+                    $('#card1').addClass('hinge data');
+                }).then(function(){
+                    setTimeout(function(){ $('#card1').hide(); }, 2000);
+                })      
+            }
+        })
+    }
 
 
 
